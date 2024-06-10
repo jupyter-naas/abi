42c5c899fdf5c16bd80227656a3f5389945fe19d
Maxime Jublou <jubloum@gmail.com>
2024-06-10 18:06:27 +0200

    feat: Make it possible to populate neo4j if empty automatically

--- apps/streamlit-chat/graph.py
+++ apps/streamlit-chat/graph.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain_community.graphs import Neo4jGraph
 from os import environ
+import pydash
 
 uri = environ.get("NEO4J_URI")
 username = environ.get("NEO4J_USERNAME")
@@ -11,3 +12,18 @@ graph = Neo4jGraph(
     username=username,
     password=password,
 )
+
+# Logic to initialize the Neo4j graph if it's empty.
+
+count_query = graph.query("""
+    MATCH (n)
+    RETURN COUNT(n) AS nodeCount            
+""")
+
+count_query = pydash.get(count_query, "[0].nodeCount")
+
+if count_query == 0:
+    print("⚙️ Populating graph database with sample data...")
+    with open('cypher_query/cypher_query.txt', 'r') as file:
+        import_query = file.read()
+        graph.query(import_query)
