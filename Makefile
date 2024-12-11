65373ffe0efeda476ebeeebc916b333815555366
Maxime Jublou <jubloum@gmail.com>
2024-12-11 10:22:09 +0100

    fix: Convert pipelines to the new way of building them using the Expose class

--- Makefile
+++ Makefile
@@ -10,8 +10,9 @@ add:
 lock:
 	docker compose run abi poetry lock --no-update
 
+path=tests/
 test:
-	docker compose run abi poetry run pytest tests/
+	docker compose run abi poetry run pytest $(path)
 
 sh: .venv
 	docker compose run -it abi bash
@@ -19,6 +20,9 @@ sh: .venv
 chat: .venv
 	docker compose run abi bash -c 'poetry install && poetry run chat-single-assistant'
 .DEFAULT_GOAL := chat
+
+api: .venv
+	docker compose run -p 9879:9879 abi poetry run api
 
 .PHONY: test
 super-assistant: .venv
--- lib/abi/utils/Expose.py
+++ lib/abi/utils/Expose.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Callable, Any
+
 from fastapi import APIRouter
 from langchain_core.tools import StructuredTool
-from typing import Callable, Any
 
 class Expose(ABC):
     
-    @staticmethod
     @abstractmethod
     def as_tools(self) -> list[StructuredTool]:
         """Returns a list of StructuredTools that can be used by an Agent.
@@ -21,28 +21,6 @@ class Expose(ABC):
         """
         raise NotImplementedError()
     
-    @staticmethod
-    @abstractmethod
-    def as_runnable(self, configuration: Any) -> Callable:
-        """Returns a callable function that executes the class's functionality with the given configuration.
-        
-        This method should be implemented by concrete classes to expose their functionality
-        as a callable function that can be executed with a predefined configuration.
-        
-        Args:
-            configuration (Any): The configuration object needed to initialize the class
-                (e.g., PipelineConfiguration for Pipeline classes)
-                
-        Returns:
-            Callable[[], Any]: A callable function that takes no arguments and executes
-                the class's functionality with the provided configuration
-            
-        Raises:
-            NotImplementedError: If the concrete class does not implement this method
-        """
-        raise NotImplementedError()
-    
-    @staticmethod
     @abstractmethod
     def as_api(self, router: APIRouter) -> None:
         """Registers API routes for the class's functionality on the provided FastAPI router.
