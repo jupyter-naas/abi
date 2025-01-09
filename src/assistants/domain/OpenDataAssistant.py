e163cf30ce55797d8d632af01e408807b7c5abf6
FlorentLvr <florent@naas.ai>
2025-01-09 18:11:37 +0100

    refactor: as_api in assistant files

--- src/assistants/domain/OpenDataAssistant.py
+++ src/assistants/domain/OpenDataAssistant.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 from abi.services.agent.Agent import Agent, AgentConfiguration, AgentSharedState, MemorySaver
 from src import secret
 from src.integrations import PerplexityIntegration
+from fastapi import APIRouter
 
 DESCRIPTION = "An Open Data Assistant that helps analyze external data sources and indicators."
 AVATAR_URL = "https://naasai-public.s3.eu-west-3.amazonaws.com/abi-demo/open_data_intelligence.png"
@@ -48,12 +49,24 @@ def create_open_data_assistant(
     if agent_shared_state is None:
         agent_shared_state = AgentSharedState()
     
-    return Agent(
+    return OpenDataAssistant(
         name="open_data_assistant", 
-        description="Use for open data analysis",
+        description=DESCRIPTION,
         chat_model=model, 
         tools=tools, 
         state=agent_shared_state, 
         configuration=agent_configuration, 
         memory=MemorySaver()
     ) 
+
+class OpenDataAssistant(Agent):
+    def as_api(
+            self, 
+            router: APIRouter, 
+            route_name: str = "open_data", 
+            name: str = "Open Data Assistant", 
+            description: str = "API endpoints to call the Open Data assistant completion.", 
+            description_stream: str = "API endpoints to call the Open Data assistant stream completion.",
+            tags: list[str] = []
+        ):
+        return super().as_api(router, route_name, name, description, description_stream, tags)
