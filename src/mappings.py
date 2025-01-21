415b1147af39e703dafe034d05d1e3d2219ba4d1
FlorentLvr <florent@naas.ai>
2025-01-21 12:14:42 +0100

    fix: pipelines & env

--- src/mappings.py
+++ src/mappings.py
@@ -5,8 +5,10 @@ COLORS_NODES = {
     'http://ontology.naas.ai/abi/Ontology': 'white',
     'http://ontology.naas.ai/abi/Integration': 'white',
     'http://ontology.naas.ai/abi/GitHubIssue': '#FFFDA2',
+    'http://ontology.naas.ai/abi/GitHubPullRequest': '#FFFDA2',
     'http://ontology.naas.ai/abi/GitHubRepository': '#47DD82',
-    'http://ontology.naas.ai/abi/GitHubUser': 'white'
+    'http://ontology.naas.ai/abi/GitHubUser': 'white',
+    'http://ontology.naas.ai/abi/TaskCompletion': 'lightblue'
 }
 
 ASSISTANTS_ORDER = {
