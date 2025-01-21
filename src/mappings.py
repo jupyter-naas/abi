10b1376a73fed05fd12cdd85c090f07bd3259162
FlorentLvr <florent@naas.ai>
2025-01-21 11:14:51 +0100

    feat: add new values to mapping

--- src/mappings.py
+++ src/mappings.py
@@ -4,6 +4,9 @@ COLORS_NODES = {
     'http://ontology.naas.ai/abi/Assistant': 'orange', 
     'http://ontology.naas.ai/abi/Ontology': 'white',
     'http://ontology.naas.ai/abi/Integration': 'white',
+    'http://ontology.naas.ai/abi/GitHubIssue': '#FFFDA2',
+    'http://ontology.naas.ai/abi/GitHubRepository': '#47DD82',
+    'http://ontology.naas.ai/abi/GitHubUser': 'white'
 }
 
 ASSISTANTS_ORDER = {
