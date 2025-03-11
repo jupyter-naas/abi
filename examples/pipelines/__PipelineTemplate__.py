15186932b97a2142a00d34779c363bef1b296cc2
FlorentLvr <florent@naas.ai>
2025-03-11 10:10:33 +0100

    refactor: modules

--- examples/pipelines/__PipelineTemplate__.py
+++ examples/pipelines/__PipelineTemplate__.py
@@ -72,7 +72,7 @@ class YourPipeline(Pipeline):
 if __name__ == "__main__":
     from abi.services.ontology_store.adaptors.secondary.OntologyStoreService__SecondaryAdaptor__Filesystem import OntologyStoreService__SecondaryAdaptor__Filesystem
     from abi.services.ontology_store.OntologyStoreService import OntologyStoreService
-    from src.core.integrations import YourIntegration, YourIntegrationConfiguration
+    from src.core.modules.common.integrations import YourIntegration, YourIntegrationConfiguration
     
     # Init store
     ontology_store = OntologyStoreService(OntologyStoreService__SecondaryAdaptor__Filesystem(store_path=config.ontology_store_path))
