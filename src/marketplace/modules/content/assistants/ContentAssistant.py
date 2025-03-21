3798c0a18cf92846d001a190a2651eed8b7d1f77
jravenel <jeremy.ravenel@cashstory.com>
2025-03-21 12:35:17 +0100

    feat: added marketplace, custom, new pages in components, and removed unused files

--- src/marketplace/modules/content/assistants/ContentAssistant.py
+++
@@ -1,5 +0,0 @@
-# Old imports
-from src.core.modules.common.integrations import LinkedInIntegration, YouTubeIntegration
-
-# New imports
-from src.core.modules.common.integrations import LinkedInIntegration, YouTubeIntegration 
