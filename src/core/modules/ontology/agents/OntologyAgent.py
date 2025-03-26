44857d0caf62e3fc0ff37c5ae844d6cf78457c8d
FlorentLvr <florent@naas.ai>
2025-03-26 18:59:51 +0100

    feat: setup workflows & pipelines to ontology agent for testing

--- src/core/modules/ontology/agents/OntologyAgent.py
+++ src/core/modules/ontology/agents/OntologyAgent.py
@@ -2,6 +2,10 @@ from abi.services.agent.Agent import Agent, AgentConfiguration, AgentSharedState
 from langchain_openai import ChatOpenAI
 from fastapi import APIRouter
 from src import secret, services
+from src.core.modules.common.integrations.NaasIntegration import NaasIntegration, NaasIntegrationConfiguration
+from src.core.modules.ontology.pipelines.AddIndividualPipeline import AddIndividualPipeline, AddIndividualPipelineConfiguration
+from src.core.modules.ontology.workflows.GetClassWorkflow import GetClassWorkflow, GetClassConfigurationWorkflow
+from src.core.modules.ontology.workflows.CreateClassOntologyYAML import CreateClassOntologyYAML, CreateClassOntologyYAMLConfiguration
 from src.core.modules.ontology.pipelines.AddSkilltoPersonPipeline import AddSkilltoPersonPipeline, AddSkilltoPersonPipelineConfiguration
 from src.core.modules.ontology.workflows.GetPersonSkillsWorkflow import GetPersonSkillsWorkflow, GetPersonSkillsConfigurationWorkflow
 
@@ -10,8 +14,14 @@ MODEL = "o3-mini"
 TEMPERATURE = 1
 AVATAR_URL = "https://naasai-public.s3.eu-west-3.amazonaws.com/abi-demo/ontology_ABI.png"
 DESCRIPTION = "A Ontology Agent that helps users to work with ontologies."
-SYSTEM_PROMPT = f"""
-You are a Ontology Agent that helps users to work with ontologies.
+SYSTEM_PROMPT = f"""You are a Ontology Agent that helps users to work with ontologies.
+You must always use your tools to perform the actions you need to do.
+You can't use your internal knowledge.
+
+Before adding or updating an individual in an ontology:
+- Search the class that could be used to add the individual using the `ontology_search_class` tool.
+- Search if the individual already exists using the `ontology_search_individual` tool.
+- If the individual doesn't exist, add it to the ontology using the `ontology_add_individual` tool.
 """
 SUGGESTIONS = []
 
@@ -32,13 +42,19 @@ def create_agent(
     )
 
     # Add pipelines
+    add_individual_pipeline = AddIndividualPipeline(AddIndividualPipelineConfiguration(ontology_store))
+    tools += add_individual_pipeline.as_tools()
+    
     add_skill_to_person_pipeline = AddSkilltoPersonPipeline(AddSkilltoPersonPipelineConfiguration(ontology_store))
     tools += add_skill_to_person_pipeline.as_tools()
 
     # Add workflows
+    get_class_workflow = GetClassWorkflow(GetClassConfigurationWorkflow(ontology_store))
+    tools += get_class_workflow.as_tools()
+
     get_person_skills_workflow = GetPersonSkillsWorkflow(GetPersonSkillsConfigurationWorkflow(ontology_store))
     tools += get_person_skills_workflow.as_tools()
-    
+
     # Use provided configuration or create default one
     if agent_configuration is None:
         agent_configuration = AgentConfiguration(system_prompt=SYSTEM_PROMPT)
