95b3c25da2dd42ecac9f79535053533e5475c4df
FlorentLvr <florent@naas.ai>
2025-03-28 11:39:27 +0100

    feat: init human resources module

--- src/core/modules/common/models/naas/Gpt4o_MiniAIModel.py
+++
@@ -1,7 +0,0 @@
-ID = "63b149d9-f8fe-4b33-8132-75e7861c347d"
-NAME = "gpt-4o-mini-2024-07-18"
-DESCRIPTION = "OpenAI's affordable and intelligent small model for fast, lightweight tasks. GPT-4o mini is cheaper and more capable than GPT-3.5 Turbo. Currently points to gpt-4o-mini-2024-07-18."
-IMAGE = "https://naasai-public.s3.eu-west-3.amazonaws.com/logos/openai_100x100.png"
-MODEL_TYPE = "chat"
-CONTEXT_WINDOW = 128000
-OWNER = "openai" 
