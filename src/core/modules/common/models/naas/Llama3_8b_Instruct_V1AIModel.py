fdd1e18c2054fd8286dcfaafbc468a8a90968905
FlorentLvr <florent@naas.ai>
2025-03-28 11:39:27 +0100

    feat: init human resources module

--- src/core/modules/common/models/naas/Llama3_8b_Instruct_V1AIModel.py
+++
@@ -1,7 +0,0 @@
-ID = "5d04a521-9c19-48cb-9350-cab63db26301"
-NAME = "meta.llama3-8b-instruct-v1:0"
-DESCRIPTION = "Ideal for limited computational power and resources, edge devices, and faster training times."
-IMAGE = "https://naasai-public.s3.eu-west-3.amazonaws.com/logos/meta_100x100.jpg"
-MODEL_TYPE = "chat"
-CONTEXT_WINDOW = 8000
-OWNER = "meta" 
