2843bf372de098800265f676db69342a9df5500e
Maxime Jublou <jubloum@gmail.com>
2025-04-03 16:50:33 +0200

    feat: Add schema loading with content comparison

--- lib/abi/services/triple_store/TripleStoreService.py
+++ lib/abi/services/triple_store/TripleStoreService.py
@@ -1,12 +1,49 @@
 from lib.abi.services.triple_store.TripleStorePorts import ITripleStoreService, ITripleStorePort, OntologyEvent
+from lib.abi.services.object_storage.ObjectStorageService import ObjectStorageService
 from rdflib import Graph, RDF, RDFS
 from typing import Callable, List, Tuple
 import uuid
 import pydash
+import hashlib
+import os
+import io
+import base64
 
-OWL_URL = "https://www.w3.org/2002/07/owl"
-CCO_URL = "https://raw.githubusercontent.com/CommonCoreOntology/CommonCoreOntologies/refs/heads/develop/src/cco-merged/CommonCoreOntologiesMerged.ttl"
-BFO_URL = "https://raw.githubusercontent.com/BFO-ontology/BFO-2020/refs/heads/master/src/owl/bfo-core.ttl"
+SCHEMA_TTL = """
+@prefix internal: <http://triple-store.internal#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+internal:Schema a owl:Class ;
+    rdfs:label "Schema" ;
+    rdfs:comment "Represents a schema file that has been loaded into the triple store" .
+
+internal:hash a owl:DatatypeProperty ;
+    rdfs:domain internal:Schema ;
+    rdfs:range xsd:string ;
+    rdfs:label "hash" ;
+    rdfs:comment "SHA-256 hash of the schema content" .
+
+internal:fileLastUpdateTime a owl:DatatypeProperty ;
+    rdfs:domain internal:Schema ;
+    rdfs:range xsd:dateTime ;
+    rdfs:label "file last update time" ;
+    rdfs:comment "Last modification timestamp of the schema file" .
+
+internal:filePath a owl:DatatypeProperty ;
+    rdfs:domain internal:Schema ;
+    rdfs:range xsd:string ;
+    rdfs:label "file path" ;
+    rdfs:comment "Path to the schema file" .
+
+internal:content a owl:DatatypeProperty ;
+    rdfs:domain internal:Schema ;
+    rdfs:range xsd:base64Binary ;
+    rdfs:label "content" ;
+    rdfs:comment "Base64 encoded content of the schema file" .
+"""
 
 class TripleStoreService(ITripleStoreService):
     """TripleStoreService provides CRUD operations and SPARQL querying capabilities for ontologies.
@@ -28,27 +65,16 @@ class TripleStoreService(ITripleStoreService):
     """
     def __init__(self, ontology_adaptor: ITripleStorePort, views: List[Tuple[str, str, str]] = [
         (None, RDF.type, None)
-    ], load_base_ontologies: bool = False):
+    ]):
         self.__ontology_adaptor = ontology_adaptor
         self.__event_listeners = {}
         self.__views = views
         
+        # Load SCHEMA_TTL in IOBuffer 
+        schema_ttl_buffer = io.StringIO(SCHEMA_TTL)
+        self.insert(Graph().parse(schema_ttl_buffer, format='turtle'))
+        
         self.init_views()
-        
-        if load_base_ontologies:
-            # Load OWL
-            self.insert(Graph().parse(OWL_URL, format='turtle'))
-            
-            # Load BFO
-            self.insert(Graph().parse(BFO_URL, format='turtle'))
-            
-                # Load CCO
-            self.insert(Graph().parse(CCO_URL, format='turtle'))
-        
-
-    def load(self, filepath: str):
-        g = Graph().parse(filepath)
-        self.insert(g)
 
     def init_views(self):
         for view in self.__views:
@@ -104,3 +130,103 @@ class TripleStoreService(ITripleStoreService):
         for topic in self.__event_listeners:
             for event_type in self.__event_listeners[topic]:
                 self.__event_listeners[topic][event_type] = pydash.filter_(self.__event_listeners[topic][event_type], lambda x: x[0] != subscription_id)
+
+    ############################################################
+    # Schema Management
+    ############################################################
+
+    def load_schema(self, filepath: str):
+        # Check if schema with filePath == filepath already exists and grab all triples
+        schema_triples = self.query(f'SELECT * WHERE {{ ?s internal:filePath "{filepath}" . }}')
+        
+        # If schema with filePath == filepath already exists, we check if the file has been modified.
+        schema_exists_in_store = len(list(schema_triples)) == 1
+        if schema_exists_in_store:
+            
+            subject = list(schema_triples)[0][0]
+            
+            # Select * from subject
+            triples = self.query(f'SELECT ?p ?o WHERE {{ <{subject}> ?p ?o . }}')
+            
+            # Load schema into a dict
+            schema_dict = {}
+            for row in triples:
+                p, o = row
+
+                schema_dict[str(p).replace('http://triple-store.internal#', '')] = str(o)
+            
+            # Get file last update time
+            file_last_update_time = os.path.getmtime(filepath)
+            
+            # If fileLastUpdateTime is the same, return. Otherwise we continue as we need to update the schema.
+            if schema_dict['fileLastUpdateTime'] == str(file_last_update_time):
+                return
+            
+            # Open file and get content.
+            with open(filepath, 'r') as file:
+                new_content = file.read()
+            
+            # Decode old content
+            old_content = base64.b64decode(schema_dict['content']).decode('utf-8')
+            
+            # Parse old and new schema
+            old_schema = Graph().parse(io.StringIO(old_content), format='turtle')
+            
+            new_schema = Graph().parse(io.StringIO(new_content), format='turtle')
+            
+            # Compute addition and deletion triples
+            addition_triples = new_schema - old_schema
+            deletion_triples = old_schema - new_schema
+            
+            # Insert addition and remove deletion triples
+            self.insert(addition_triples)
+            self.remove(deletion_triples)
+            
+            # Update schema information in the triple store.
+            
+            self.remove(Graph().parse(io.StringIO(f'''
+                @prefix internal: <http://triple-store.internal#> .
+                
+                <{subject}> internal:hash "{schema_dict['hash']}" ;
+                    internal:fileLastUpdateTime "{schema_dict['fileLastUpdateTime']}" ;
+                    internal:content "{schema_dict['content']}" .
+            '''), format='turtle'))
+            
+            self.insert(Graph().parse(io.StringIO(f'''
+                @prefix internal: <http://triple-store.internal#> .
+                
+                <{subject}> internal:hash "{hashlib.sha256(new_content.encode('utf-8')).hexdigest()}" ;
+                    internal:fileLastUpdateTime "{file_last_update_time}" ;
+                    internal:content "{base64.b64encode(new_content.encode('utf-8')).decode('utf-8')}" .
+            '''), format='turtle'))
+            
+            # Return as we don't need to continue as we have already updated the schema.
+            return
+        elif not schema_exists_in_store:
+
+            # Open file and get content.
+            with open(filepath, 'r') as file:
+                content = file.read()
+            
+            # Compute base64 content
+            base64_content = base64.b64encode(content.encode('utf-8')).decode('utf-8')
+                
+            # Compute hash of content
+            content_hash = hashlib.sha256(content.encode('utf-8')).hexdigest()
+            
+            # Parse schema
+            g = Graph().parse(filepath)
+            
+            # Insert schema into the triple store
+            self.insert(g)
+            
+            # Insert Schema with hash, filePath, fileLastUpdateTime and content to be able to track changes.
+            self.insert(Graph().parse(io.StringIO(f'''
+                @prefix internal: <http://triple-store.internal#> .
+                                                
+                <http://triple-store.internal/{uuid.uuid4()}> a internal:Schema ;
+                    internal:hash "{content_hash}" ;
+                    internal:filePath "{filepath}" ;
+                    internal:fileLastUpdateTime "{file_last_update_time}" ;
+                    internal:content "{base64_content}" .
+            '''), format='turtle'))
