152d59560faeafcd4bb82cb6a28360d00eb58cd2
FlorentLvr <florent@naas.ai>
2025-04-03 18:51:50 +0200

    feat: add pipelines

--- src/core/modules/ontology/pipelines/AddSkillPipeline.py
+++ src/core/modules/ontology/pipelines/AddSkillPipeline.py
@@ -4,8 +4,8 @@ from langchain_core.tools import StructuredTool
 from dataclasses import dataclass
 from fastapi import APIRouter
 from pydantic import Field
-from typing import Optional
-from rdflib import Literal, Graph, Namespace
+from typing import Optional, List
+from rdflib import Literal, Graph, URIRef
 from src.core.modules.ontology.pipelines.AddIndividualPipeline import (
     AddIndividualPipeline,
     AddIndividualPipelineConfiguration,
@@ -25,8 +25,10 @@ class AddSkillPipelineConfiguration(PipelineConfiguration):
     add_individual_pipeline_configuration: AddIndividualPipelineConfiguration
 
 class AddSkillPipelineParameters(PipelineParameters):
-    name: str = Field(..., description="Name of the skill (e.g. 'Python Programming')")
+    name: Optional[str] = Field(None, description="Name of the skill (e.g. 'Python Programming') to be added in class: https://www.commoncoreontologies.org/ont00000089")
+    individual_uri: Optional[str] = Field(None, description="URI of the skill if already known. It must start with 'http://ontology.naas.ai/abi/'.")
     description: Optional[str] = Field(None, description="Description of the skill")
+    person_uris: Optional[List[str]] = Field(None, description="List of person URIs from class: https://www.commoncoreontologies.org/ont00001262. URIs must start with 'http://ontology.naas.ai/abi/'.")
 
 class AddSkillPipeline(Pipeline):
     """Pipeline for adding a new skill to the ontology."""
@@ -38,18 +40,34 @@ class AddSkillPipeline(Pipeline):
         self.__add_individual_pipeline = AddIndividualPipeline(configuration.add_individual_pipeline_configuration)
 
     def run(self, parameters: AddSkillPipelineParameters) -> str:
-        # Create skill URI using the name
-        skill_uri, graph = self.__add_individual_pipeline.run(AddIndividualPipelineParameters(
-            class_uri=CCO.ont00000089,
-            individual_label=parameters.name
-        ))
+        # Initialize a new graph
+        graph = Graph()
 
-        # Initialize a new graph for performance
-        graph.bind("dcterms", DCTERMS)
+        # Create skill URI using the name
+        skill_uri = parameters.individual_uri
+        if parameters.name and not skill_uri:
+            skill_uri, graph = self.__add_individual_pipeline.run(AddIndividualPipelineParameters(
+                class_uri=CCO.ont00000089,
+                individual_label=parameters.name
+            ))
+        else:
+            if skill_uri.startswith("http://ontology.naas.ai/abi/"):
+                skill_uri = URIRef(skill_uri)
+            else:
+                raise ValueError(f"Invalid Skill URI: {skill_uri}. It must start with 'http://ontology.naas.ai/abi/'.")
 
         # Add skill properties
         if parameters.description:
             graph.add((skill_uri, DCTERMS.description, Literal(parameters.description)))
+
+        # Add person URIs
+        if parameters.person_uris:
+            for person_uri in parameters.person_uris:
+                if person_uri.startswith("http://ontology.naas.ai/abi/"):
+                    graph.add((skill_uri, CCO.isSkillOf, URIRef(person_uri)))
+                    graph.add((URIRef(person_uri), CCO.hasSkill, skill_uri))
+                else:
+                    raise ValueError(f"Invalid Person URI: {person_uri}. It must start with 'http://ontology.naas.ai/abi/'.")
         
         # Save the graph
         self.__configuration.triple_store.insert(graph)
