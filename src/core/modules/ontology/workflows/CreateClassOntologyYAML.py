3e6309d7a754ca1df9f71b10fba1ff501872852a
FlorentLvr <florent@naas.ai>
2025-04-04 11:41:37 +0200

    refactor: rename description in python file

--- src/core/modules/ontology/workflows/CreateClassOntologyYAML.py
+++ src/core/modules/ontology/workflows/CreateClassOntologyYAML.py
@@ -41,7 +41,7 @@ class CreateClassOntologyYAMLParameters(WorkflowParameters):
     """
     ontology_name: str = Field(..., description="The name of the ontology store to use")
     label: str = Field(..., description="The label of the ontology")
-    description: str = Field(..., description="The description of the ontology. Example: 'Represents ABI Ontology with assistants, workflows, ontologies, pipelines and integrations.'")
+    description: str = Field(..., description="The description of the ontology. Example: 'Represents ABI Ontology with agents, workflows, ontologies, pipelines and integrations.'")
     logo_url: str = "https://naasai-public.s3.eu-west-3.amazonaws.com/abi-demo/ontology_ULO.png"
     level: str ='USE_CASE'
     display_relations_names: bool = True
