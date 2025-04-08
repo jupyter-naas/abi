64391a99eaa305492c267dff870b260b950c0b0e
Maxime Jublou <jubloum@gmail.com>
2025-04-08 15:43:57 +0200

    fix: Handle background=True for Module trigger auto loading

--- src/__init__.py
+++ src/__init__.py
@@ -89,8 +89,12 @@ logger.debug("Loading triggers")
 for module in modules:
     # Loading triggers
     for trigger in module.triggers:
-        topic, event_type, callback = trigger
-        services.triple_store_service.subscribe(topic, event_type, callback)
+        if len(trigger) == 3:
+            topic, event_type, callback = trigger
+            services.triple_store_service.subscribe(topic, event_type, callback)
+        elif len(trigger) == 4:
+            topic, event_type, callback, background = trigger
+            services.triple_store_service.subscribe(topic, event_type, callback, background)
 
     # Loading ontologies
     for ontology in module.ontologies:
