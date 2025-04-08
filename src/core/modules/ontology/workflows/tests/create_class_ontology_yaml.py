93bf74e2091a30529153ddd742dc0111682142b6
FlorentLvr <florent@naas.ai>
2025-04-08 11:56:22 +0200

    fix: create class yaml ontology

---
+++ src/core/modules/ontology/workflows/tests/create_class_ontology_yaml.py
@@ -0,0 +1,17 @@
+from src.core.modules.naas.integrations.NaasIntegration import NaasIntegration, NaasIntegrationConfiguration
+from src.core.modules.ontology.workflows.CreateClassOntologyYAML import CreateClassOntologyYAMLWorkflow, CreateClassOntologyYAMLConfiguration, CreateClassOntologyYAMLParameters
+from src import services, secret
+
+# Configuration
+naas_integration_config = NaasIntegrationConfiguration(api_key=secret.get("NAAS_API_KEY"))
+workflow = CreateClassOntologyYAMLWorkflow(CreateClassOntologyYAMLConfiguration(naas_integration_config, services.triple_store_service))
+
+# Parameters
+parameters = CreateClassOntologyYAMLParameters(
+    class_uri="https://www.commoncoreontologies.org/ont00001262",
+    label="Person",
+    description="Person Ontology",
+)
+
+# Run workflow
+workflow.graph_to_yaml(parameters)
