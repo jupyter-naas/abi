976c8d437cf1c29ce0858ce214ba5a6f38c9ab05
Maxime Jublou <jubloum@gmail.com>
2025-04-09 15:55:34 +0200

    fix: Rename target into triplestore-prod-*

--- scripts/triplestore_prod_override.py
+++ scripts/triplestore_prod_override.py
@@ -17,4 +17,5 @@ if __name__ == "__main__":
     backup_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     os.system(f'AWS_ACCESS_KEY_ID={access_key_id} AWS_SECRET_ACCESS_KEY={secret_access_key} AWS_SESSION_TOKEN={session_token} aws s3 mv --recursive {bucket}/ontologies {bucket}/ontologies-backups/{backup_timestamp}')
     
-    os.system(f'AWS_ACCESS_KEY_ID={access_key_id} AWS_SECRET_ACCESS_KEY={secret_access_key} AWS_SESSION_TOKEN={session_token} aws s3 cp --recursive {bucket}/storage/triplestore {bucket}/ontologies')
+    # os.system(f'AWS_ACCESS_KEY_ID={access_key_id} AWS_SECRET_ACCESS_KEY={secret_access_key} AWS_SESSION_TOKEN={session_token} aws s3 cp --recursive {bucket}/storage/triplestore {bucket}/ontologies')
+    os.system(f'AWS_ACCESS_KEY_ID={access_key_id} AWS_SECRET_ACCESS_KEY={secret_access_key} AWS_SESSION_TOKEN={session_token} aws s3 sync --follow-symlinks --delete  storage/triplestore {bucket}/ontologies')
