1e833df281714f8fb2fc5428541e14812c927b8e
FlorentLvr <florent@naas.ai>
2025-04-10 17:05:45 +0200

    fix: add prefix to sparql

--- src/core/modules/ontology/workflows/SearchCommercialOrganizationWorkflow.py
+++ src/core/modules/ontology/workflows/SearchCommercialOrganizationWorkflow.py
@@ -27,6 +27,8 @@ class SearchCommercialOrganizationWorkflow(Workflow):
     def search_commercial_organization(self, parameters: SearchCommercialOrganizationWorkflowParameters) -> dict:
         class_uri = "https://www.commoncoreontologies.org/ont00000443"  # URI for Commercial Organization
         query = f"""
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         SELECT DISTINCT ?class_uri ?individual_uri ?label (MAX(?temp_score) AS ?score)
         WHERE {{
             # Filter On Class URI and ensure individual is a NamedIndividual
