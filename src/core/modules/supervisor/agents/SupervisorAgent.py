5eadaf6c3526b177a45e368b085087fbe15ad512
FlorentLvr <florent@naas.ai>
2025-05-12 16:26:01 +0200

    refactor: print message

--- src/core/modules/supervisor/agents/SupervisorAgent.py
+++ src/core/modules/supervisor/agents/SupervisorAgent.py
@@ -25,12 +25,6 @@ AVATAR_URL = (
 )
 DESCRIPTION = "Coordinates and manages specialized agents."
 SYSTEM_PROMPT = f"""You are ABI, an advanced orchestrator agent designed to coordinate multiple specialized agents.
-
-General Rules
---------------------------------
-- You MUST always include the agent and tool used at the beginning of the report in human readable format as follow: '> {{Agent Name}} - {{Tool Name}}' + 2 blank lines (e.g. '> Ontology Agent - Search Class\n\n' for agent: ontology_agent, tool: ontology_search_class)
-- If you can't delegate the task, you can create a feature request using the 'support_agent_create_feature_request' tool. You MUST validate the need of the user request before creating a feature request. Return the issue html URL in the response.
-- If an error occurs, you MUST use the 'support_agent_create_bug_report' tool to create a bug report. Return the issue html URL in the response.
 - Return URL links as follow: [Link](https://www.google.com)
 - Return Images as follow: ![Image](https://www.google.com/image.png)
 - You MUST always adapt your language to the user request. If user request is written in french, you MUST answer in french.
@@ -67,8 +61,8 @@ def create_agent(
     if agent_configuration is None:
         agent_configuration = AgentConfiguration(
             system_prompt=SYSTEM_PROMPT,
-            on_tool_usage=lambda x: print(x),
-            on_tool_response=lambda x: print(x),
+            on_tool_usage=lambda x: print("Tool usage:", x),
+            on_tool_response=lambda x: print("Tool response:", x),
         )
     if agent_shared_state is None:
         agent_shared_state = AgentSharedState(thread_id=0)
