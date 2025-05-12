6e782533370524afb1dac4b7b1a23d9a9b03b0dc
Maxime Jublou <jubloum@gmail.com>
2025-05-12 17:46:07 +0200

    fix: lib.abi mypy errors

--- lib/abi/services/secret/adaptors/secondary/dotenv_secret_secondaryadaptor.py
+++ lib/abi/services/secret/adaptors/secondary/dotenv_secret_secondaryadaptor.py
@@ -1,11 +1,11 @@
 from abi.services.secret.SecretPorts import ISecretAdapter
 from dotenv import load_dotenv
 import os
-
+from typing import Any
 
 class DotenvSecretSecondaryAdaptor(ISecretAdapter):
     def __init__(self):
         load_dotenv()
 
-    def get(self, key: str, default: any = None) -> str:
+    def get(self, key: str, default: Any = None) -> str:
         return os.getenv(key, default)
