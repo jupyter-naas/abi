18306a8216939769f8d9e0c39d748887f7fd68af
Maxime Jublou <jubloum@gmail.com>
2025-05-18 22:31:53 +0200

    feat: Add aws bedrock example

---
+++ src/custom/modules/aws-bedrock-anthropic-claude-3-5-sonnet/models/aws_bedrock_anthropic_clause_3_5_sonnet.py
@@ -0,0 +1,16 @@
+from lib.abi.models.Model import ChatModel
+
+from langchain_aws import ChatBedrockConverse
+
+
+model = ChatModel(
+    id="1ef84540-0000-4000-8000-000000000000",
+    name="AWS Bedrock Anthropic Claude 3.5 Sonnet",
+    description="AWS Bedrock Anthropic Claude 3.5 Sonnet",
+    image="",
+    context_window=200000,
+    owner="aws-bedrock",
+    model=ChatBedrockConverse(
+        model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
+    )
+)
