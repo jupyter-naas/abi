4c38c8ba1f4b0bf5d206bbc6e2d0b06e897275c6
Maxime Jublou <jubloum@gmail.com>
2025-05-19 09:06:39 +0200

    fix: Gemini

--- src/custom/modules/google-gemini-2-0-flash/agents/google_gemini_2_0_flash.py
+++ src/custom/modules/google-gemini-2-0-flash/agents/google_gemini_2_0_flash.py
@@ -6,11 +6,10 @@ def create_agent():
     class GoogleGemini2FlashAgent(Agent): pass
     
     return GoogleGemini2FlashAgent(
-        id=model.id,
         name=model.name,
         description="Google's most advanced, multimodal flagship model that's cheaper and faster than GPT-4 Turbo.",
         chat_model=model.model,
         configuration=AgentConfiguration(
-            system_prompt="You are a helpful assistant that can answer questions and help with tasks."
+            system_prompt="You are Gemini, a helpful assistant that can answer questions and help with tasks."
         )
     )
