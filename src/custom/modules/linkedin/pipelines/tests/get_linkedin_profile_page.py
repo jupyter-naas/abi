68fa15a276d16477328feaeae5d69bf44120fc23
FlorentLvr <florent@naas.ai>
2025-05-28 14:25:39 +0200

    fix: check ruff

--- src/custom/modules/linkedin/pipelines/tests/get_linkedin_profile_page.py
+++ src/custom/modules/linkedin/pipelines/tests/get_linkedin_profile_page.py
@@ -5,36 +5,17 @@ import pydash
 from abi.utils.Storage import save_json, save_triples, save_yaml
 import os
 from rdflib import Graph, URIRef, RDF, OWL, RDFS, Literal, XSD, Namespace
-from src.core.modules.ontology.workflows.SearchIndividualWorkflow import (
-    SearchIndividualWorkflowConfiguration,
-)
-from src.core.modules.ontology.pipelines.AddIndividualPipeline import (
-    AddIndividualPipelineConfiguration,
-    AddIndividualPipeline,
-)
 from src import services
-import hashlib
-import uuid
 from src.core.modules.ontology.mappings import COLORS_NODES
 from abi.utils.OntologyYaml import OntologyYaml
 from datetime import datetime
-import calendar
+from abi.utils.String import string_to_uuid
+import uuid
+from src.custom.modules.linkedin.utils.LinkedIn import get_date
 
 ABI = Namespace("http://ontology.naas.ai/abi/")
 LINKEDIN = Namespace("http://ontology.naas.ai/abi/linkedin#")
 BFO = Namespace("http://purl.obolibrary.org/obo/")
-
-def string_to_uuid(input_string):
-    # Create a SHA-256 hash object
-    hash_object = hashlib.sha256(input_string.encode())
-    
-    # Get the hexadecimal representation of the hash
-    hex_hash = hash_object.hexdigest()
-    
-    # Use the first 32 characters of the hex hash to create a UUID
-    uuid_obj = uuid.UUID(hex_hash[:32])
-    
-    return uuid_obj
 
 # Configuration
 ## Integration
@@ -48,48 +29,59 @@ if not li_at or not JSESSIONID:
 configuration = LinkedInIntegrationConfiguration(li_at=li_at, JSESSIONID=JSESSIONID)
 linkedin_integration = LinkedInIntegration(configuration)
 
-## Pipeline
-search_individual_workflow_configuration = SearchIndividualWorkflowConfiguration(
-    services.triple_store_service,
-)
-add_individual_pipeline_configuration = AddIndividualPipelineConfiguration(
-    services.triple_store_service,
-    search_individual_workflow_configuration
-)
-add_individual_pipeline = AddIndividualPipeline(add_individual_pipeline_configuration)
-
-# Parameters
-linkedin_url = "https://www.linkedin.com/in/jeremyravenel/"
+## Parameters
+linkedin_url = "https://www.linkedin.com/in/giacomodecolle/"
 data_store_path = "datastore/linkedin/get_profile_view"
 
 # Integration
 linkedin_data = linkedin_integration.get_profile_view(linkedin_url)
 profile_id = linkedin_integration.get_profile_id(linkedin_url)
+
+# Pipeline
 output_dir = os.path.join(data_store_path, profile_id)
 data = linkedin_data.get("data", {})
 included = linkedin_data.get("included", [])
 
 ## Add LinkedIn Profile Page
-### Data properties
 entity_urn = data.get("entityUrn")
 linkedin_id = entity_urn.split(":")[-1]
 linkedin_url = f"https://www.linkedin.com/in/{linkedin_id}"
-linkedin_public_id = ""
-linkedin_public_url = ""
+linkedin_public_id = None
+linkedin_public_url = None
 if linkedin_id.startswith("ACo"):
     linkedin_public_id = profile_id
     linkedin_public_url = linkedin_url
-page_uri = ABI[str(string_to_uuid(linkedin_id))]
-print(f"Page URI: {page_uri}")
+linkedin_page_uri = ABI[str(string_to_uuid(linkedin_id))]
+print(f"==> Create LinkedIn Profile Page: {linkedin_id} ({linkedin_page_uri})")
+graph_linkedin_page = Graph()
+try:
+    graph_linkedin_page = services.triple_store_service.get_subject_graph(linkedin_page_uri)
+except Exception:
+    graph_linkedin_page.add((linkedin_page_uri, RDF.type, OWL.NamedIndividual))
+    graph_linkedin_page.add((linkedin_page_uri, RDF.type, LINKEDIN.LinkedInProfilePage))
+    graph_linkedin_page.add((linkedin_page_uri, RDFS.label, Literal(profile_id)))
 
-### Add profile data
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, RDFS.label, Literal(profile_id))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, RDFS.label, Literal(profile_id)))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.entityUrn, Literal(entity_urn))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.entityUrn, Literal(entity_urn)))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.linkedin_id, Literal(linkedin_id))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.linkedin_id, Literal(linkedin_id)))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.linkedin_url, Literal(linkedin_url))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.linkedin_url, Literal(linkedin_url)))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.linkedin_public_id, Literal(linkedin_public_id))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.linkedin_public_id, Literal(linkedin_public_id)))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.linkedin_public_url, Literal(linkedin_public_url))))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.linkedin_public_url, Literal(linkedin_public_url)))
+
+### Add LinkedIn Profile
 profile_urn = data.get("*profile")
 lk_profile_id = str(string_to_uuid(profile_urn))
 profile_uri = ABI[lk_profile_id]
 profile_data = pydash.filter_(included, lambda x: x.get("entityUrn") == profile_urn)[0]
 save_json(dict(sorted(profile_data.items())), output_dir, f"{profile_id}_Profile_{lk_profile_id}.json", copy=False)
-name = profile_data.get("firstName", "") + " " + profile_data.get("lastName", "")
-print(f"Profile URI: {profile_uri}")
+name = (profile_data.get("firstName", "") + " " + profile_data.get("lastName", "")).strip()
+print(f"==> Create LinkedIn Profile: {name} ({profile_uri})")
 graph_profile_data = Graph()
 try:
     graph_profile_data = services.triple_store_service.get_subject_graph(profile_uri)
@@ -98,56 +90,11 @@ except Exception:
     graph_profile_data.add((profile_uri, RDF.type, URIRef("http://ontology.naas.ai/abi/linkedin#LinkedInProfile")))
     graph_profile_data.add((profile_uri, RDFS.label, Literal(name)))
 
-#### Add properties
-is_profile_of_exists = False
-for s, p, o in graph_profile_data:
-    if str(s) == str(ABI.isProfileOf) and str(p) == str(page_uri):
-        is_profile_of_exists = True
-        break
-if not is_profile_of_exists:
-    graph_profile_data.add((profile_uri, ABI.isProfileOf, page_uri))
-
-def get_last_day_of_month(year, month):
-    # Check if the month is valid
-    if month < 1 or month > 12:
-        raise ValueError("Month must be between 1 and 12")
-
-    # Use calendar.monthrange to get the last day of the month
-    last_day = calendar.monthrange(year, month)[1]
-    return last_day
-
-def get_date(data, date_type):
-    date_iso = None
-    date_format_iso = "%Y-%m-%dT%H:%M:%S.%fZ"
-    m = "01"
-    d = "01"
-    H = "00"
-    M = "00"
-    S = "00"
-    if date_type == "end":
-        m = "12"
-        d = "31"
-        H = "23"
-        M = "59"
-        S = "59"
-    if data:
-        year = data.get("year")
-        month = data.get("month")
-        day = data.get("day")
-        if year and not month and not day:
-            date_iso = datetime.strptime(
-                f"{year}-{m}-{d}T{H}:{M}:{S}.000Z", date_format_iso
-            )
-        elif year and month and not day:
-            date_iso = datetime.strptime(
-                f"{year}-{month}-{get_last_day_of_month(year, month)}T{H}:{M}:{S}.000Z",
-                date_format_iso,
-            )
-        elif year and month and day:
-            date_iso = datetime.strptime(
-                f"{year}-{month}-{day}T{H}:{M}:{S}.000Z", date_format_iso
-            )
-    return date_iso.strftime(date_format_iso)
+#### Add object properties
+if len(list(graph_profile_data.triples((profile_uri, ABI.isProfileOf, linkedin_page_uri)))) == 0:
+    graph_profile_data.add((profile_uri, ABI.isProfileOf, linkedin_page_uri))
+if len(list(graph_linkedin_page.triples((linkedin_page_uri, LINKEDIN.hasProfile, profile_uri)))) == 0:
+    graph_linkedin_page.add((linkedin_page_uri, LINKEDIN.hasProfile, profile_uri))
 
 def add_properties(graph: Graph, uri: URIRef, data: dict) -> Graph:
     """Add data properties to an RDF graph.
@@ -185,6 +132,7 @@ def add_properties(graph: Graph, uri: URIRef, data: dict) -> Graph:
                 except Exception:
                     graph_item.add((item_uri, RDF.type, OWL.NamedIndividual))
                     graph_item.add((item_uri, RDF.type, LINKEDIN[class_label]))
+                    graph_item.add((item_uri, RDFS.label, Literal(item)))
 
                 check_relation = graph.triples((uri, LINKEDIN[relation_label], item_uri))
                 if len(list(check_relation)) == 0:
@@ -233,37 +181,35 @@ def add_properties(graph: Graph, uri: URIRef, data: dict) -> Graph:
 
         # Add TimePeriod as object properties: TimePeriod
         if k == "timePeriod" and isinstance(v, dict):
-            start_date = v.get("startDate", {})
-            end_date = v.get("endDate", {})
-            if start_date:
-                date_label = get_date(start_date, "start")
-                date_epoch = int(datetime.strptime(date_label, "%Y-%m-%dT%H:%M:%S.%fZ").timestamp() * 1000)
+            start_date = get_date(v.get("startDate", {}), "start")
+            end_date = get_date(v.get("endDate", {}), "end")
+            if start_date is not None:
+                date_epoch = int(datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S.%fZ").timestamp() * 1000)
                 date_uri = ABI[str(date_epoch)]
                 graph_date = Graph()
                 try:
                     graph_date = services.triple_store_service.get_subject_graph(date_uri)
-                    print(f"🔍 - Date already exists: {date_label} ({date_uri})")
+                    print(f"🔍 - Date already exists: {start_date} ({date_uri})")
                 except Exception:
                     graph_date.add((date_uri, RDF.type, OWL.NamedIndividual))
                     graph_date.add((date_uri, RDF.type, URIRef(ABI.ISO8601UTCDateTime)))
-                    graph_date.add((date_uri, RDFS.label, Literal(date_label, datatype=XSD.dateTime)))
+                    graph_date.add((date_uri, RDFS.label, Literal(start_date, datatype=XSD.dateTime)))
                 
                 check_relation = graph.triples((uri, BFO.BFO_0000222, date_uri))
                 if len(list(check_relation)) == 0:
                     graph.add((uri, BFO.BFO_0000222, date_uri))
                 graph += graph_date
-            if end_date:
-                date_label = get_date(end_date, "end")
-                date_epoch = int(datetime.strptime(date_label, "%Y-%m-%dT%H:%M:%S.%fZ").timestamp() * 1000)
+            if end_date is not None:
+                date_epoch = int(datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S.%fZ").timestamp() * 1000)
                 date_uri = ABI[str(date_epoch)]
                 graph_date = Graph()
                 try:
                     graph_date = services.triple_store_service.get_subject_graph(date_uri)
-                    print(f"🔍 - Date already exists: {date_label} ({date_uri})")
+                    print(f"🔍 - Date already exists: {end_date} ({date_uri})")
                 except Exception:
                     graph_date.add((date_uri, RDF.type, OWL.NamedIndividual))
                     graph_date.add((date_uri, RDF.type, URIRef(ABI.ISO8601UTCDateTime)))
-                    graph_date.add((date_uri, RDFS.label, Literal(date_label, datatype=XSD.dateTime)))
+                    graph_date.add((date_uri, RDFS.label, Literal(end_date, datatype=XSD.dateTime)))
                 check_relation_inverse = graph.triples((date_uri, BFO.BFO_0000222, uri))
                 if len(list(check_relation_inverse)) == 0:
                     graph_date.add((date_uri, BFO.BFO_0000224, uri))
@@ -301,53 +247,6 @@ for k, v in data.items():
             graph_profile_data.add((profile_uri, LINKEDIN[relation_label], view_uri))
             graph_profile_data += graph_view_data
 
-### Add Triples to LinkedIn Profile Page
-graph_linkedin_page = Graph()
-try:
-    graph_linkedin_page = services.triple_store_service.get_subject_graph(page_uri)
-except Exception:
-    graph_linkedin_page.add((page_uri, RDF.type, OWL.NamedIndividual))
-    graph_linkedin_page.add((page_uri, RDF.type, LINKEDIN.LinkedInProfilePage))
-
-entity_urn_exists = False
-linkedin_page_label_exists = False
-linkedin_id_exists = False
-linkedin_url_exists = False
-linkedin_public_id_exists = False
-linkedin_public_url_exists = False
-linkedin_profile_exists = False
-
-for s, p, o in graph_linkedin_page:
-    if str(s) == str(RDFS.label) and str(p) == str(profile_id):
-        linkedin_page_label_exists = True
-    if str(s) == str(LINKEDIN.entityUrn) and str(p) == str(entity_urn):
-        entity_urn_exists = True
-    if str(s) == str(LINKEDIN.linkedin_id) and str(p) == str(linkedin_id):
-        linkedin_id_exists = True
-    if str(s) == str(LINKEDIN.linkedin_url) and str(p) == str(linkedin_url):
-        linkedin_url_exists = True
-    if str(s) == str(LINKEDIN.linkedin_public_id) and str(p) == str(linkedin_public_id):
-        linkedin_public_id_exists = True
-    if str(s) == str(LINKEDIN.linkedin_public_url) and str(p) == str(linkedin_public_url):
-        linkedin_public_url_exists = True
-    if str(s) == str(LINKEDIN.hasProfile) and str(p) == str(profile_uri):
-        linkedin_profile_exists = True
-
-if not linkedin_page_label_exists:
-    graph_linkedin_page.add((page_uri, RDFS.label, Literal(profile_id)))
-if not entity_urn_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.entityUrn, Literal(entity_urn)))
-if not linkedin_id_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.linkedin_id, Literal(linkedin_id)))
-if not linkedin_url_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.linkedin_url, Literal(linkedin_url)))
-if not linkedin_public_id_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.linkedin_public_id, Literal(linkedin_public_id)))
-if not linkedin_public_url_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.linkedin_public_url, Literal(linkedin_public_url)))
-if not linkedin_profile_exists:
-    graph_linkedin_page.add((page_uri, LINKEDIN.hasProfile, profile_uri))
-
 # Save triples
 graph_insert = Graph()
 graph_insert.bind("abi", ABI)
@@ -356,6 +255,7 @@ graph_insert.bind("rdfs", RDFS)
 graph_insert.bind("xsd", XSD)
 graph_insert.bind("rdf", RDF)
 graph_insert.bind("linkedin", LINKEDIN)
+graph_insert.bind("bfo", BFO)
 graph_insert += graph_linkedin_page
 graph_insert += graph_profile_data
 save_triples(
