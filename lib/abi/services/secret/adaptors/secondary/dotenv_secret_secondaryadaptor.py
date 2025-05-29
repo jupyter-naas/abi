09355c7afbb5308746ab28fb25cce76737fe9848
Maxime Jublou <jubloum@gmail.com>
2025-05-29 18:29:35 +0200

    feat: Update secret management

--- lib/abi/services/secret/adaptors/secondary/dotenv_secret_secondaryadaptor.py
+++ lib/abi/services/secret/adaptors/secondary/dotenv_secret_secondaryadaptor.py
@@ -1,7 +1,7 @@
 from abi.services.secret.SecretPorts import ISecretAdapter
 from dotenv import load_dotenv
 import os
-from typing import Any
+from typing import Any, Dict
 
 
 class DotenvSecretSecondaryAdaptor(ISecretAdapter):
@@ -10,3 +10,15 @@ class DotenvSecretSecondaryAdaptor(ISecretAdapter):
 
     def get(self, key: str, default: Any = None) -> str:
         return os.getenv(key, default)
+
+    def set(self, key: str, value: str):
+        os.environ[key] = value
+
+    def remove(self, key: str):
+        os.environ.pop(key, None)
+
+    def list(self) -> Dict[str, str | None]:
+        secrets: Dict[str, str | None] = {}
+        for key in os.environ.keys():
+            secrets[str(key)] = str(os.getenv(key))
+        return secrets
