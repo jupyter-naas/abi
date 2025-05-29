073f2ff7ddfbe3c89f39b68454fc86d462a7543a
Maxime Jublou <jubloum@gmail.com>
2025-05-29 09:40:28 +0200

    feat: Add AWSNeptune secondary adapter to triple store

--- lib/abi/services/triple_store/adaptors/secondary/TripleStoreService__SecondaryAdaptor__Filesystem.py
+++ lib/abi/services/triple_store/adaptors/secondary/TripleStoreService__SecondaryAdaptor__Filesystem.py
@@ -10,6 +10,7 @@ from rdflib import Graph, RDFS, query, URIRef, BNode
 import rdflib
 from typing import List, Dict, Tuple, Any
 import os
+from threading import Lock
 
 
 class TripleStoreService__SecondaryAdaptor__Filesystem(
@@ -20,9 +21,12 @@ class TripleStoreService__SecondaryAdaptor__Filesystem(
 
     __live_graph: Graph
 
+    __lock: Lock
+
     def __init__(self, store_path: str, triples_path: str = "triples"):
         self.__store_path = store_path
         self.__triples_path = triples_path
+        self.__lock = Lock()
 
         os.makedirs(os.path.join(self.__store_path, self.__triples_path), exist_ok=True)
 
@@ -45,58 +49,61 @@ class TripleStoreService__SecondaryAdaptor__Filesystem(
     ## File System Methods
 
     def insert(self, triples: Graph):
-        triples_by_subject: Dict[Any, List[Tuple[Any, Any]]] = self.triples_by_subject(
-            triples
-        )
-
-        for subject in triples_by_subject:
-            subject_hash = self.iri_hash(subject)
-
-            if not os.path.exists(self.hash_triples_path(subject_hash)):
-                graph = Graph()
-
-                for prefix, namespace in triples.namespaces():
-                    graph.bind(prefix, namespace)
-            else:
-                graph = Graph().parse(
-                    self.hash_triples_path(subject_hash), format="turtle"
-                )
-
-            for p, o in triples_by_subject[subject]:
-                graph.add((subject, p, o))
-
-            graph.serialize(
-                destination=self.hash_triples_path(subject_hash), format="turtle"
+        with self.__lock:
+            triples_by_subject: Dict[Any, List[Tuple[Any, Any]]] = (
+                self.triples_by_subject(triples)
             )
 
-        for prefix, namespace in triples.namespaces():
-            self.__live_graph.bind(prefix, namespace)
+            for subject in triples_by_subject:
+                subject_hash = self.iri_hash(subject)
 
-        # Update the live graph
-        self.__live_graph += triples
+                if not os.path.exists(self.hash_triples_path(subject_hash)):
+                    graph = Graph()
 
-    def remove(self, triples: Graph):
-        triples_by_subject: Dict[Any, List[Tuple[Any, Any]]] = self.triples_by_subject(
-            triples
-        )
-
-        for subject in triples_by_subject:
-            subject_hash = self.iri_hash(subject)
-
-            if os.path.exists(self.hash_triples_path(subject_hash)):
-                graph = Graph().parse(
-                    self.hash_triples_path(subject_hash), format="turtle"
-                )
+                    for prefix, namespace in triples.namespaces():
+                        graph.bind(prefix, namespace)
+                else:
+                    graph = Graph().parse(
+                        self.hash_triples_path(subject_hash), format="turtle"
+                    )
 
                 for p, o in triples_by_subject[subject]:
-                    graph.remove((subject, p, o))
+                    graph.add((subject, p, o))
 
                 graph.serialize(
                     destination=self.hash_triples_path(subject_hash), format="turtle"
                 )
 
-        # Update the live graph
-        self.__live_graph -= triples
+            for prefix, namespace in triples.namespaces():
+                self.__live_graph.bind(prefix, namespace)
+
+            # Update the live graph
+            self.__live_graph += triples
+
+    def remove(self, triples: Graph):
+        with self.__lock:
+            triples_by_subject: Dict[Any, List[Tuple[Any, Any]]] = (
+                self.triples_by_subject(triples)
+            )
+
+            for subject in triples_by_subject:
+                subject_hash = self.iri_hash(subject)
+
+                if os.path.exists(self.hash_triples_path(subject_hash)):
+                    graph = Graph().parse(
+                        self.hash_triples_path(subject_hash), format="turtle"
+                    )
+
+                    for p, o in triples_by_subject[subject]:
+                        graph.remove((subject, p, o))
+
+                    graph.serialize(
+                        destination=self.hash_triples_path(subject_hash),
+                        format="turtle",
+                    )
+
+            # Update the live graph
+            self.__live_graph -= triples
 
     ## Ontology Methods
 
@@ -112,49 +119,52 @@ class TripleStoreService__SecondaryAdaptor__Filesystem(
         raise Exceptions.SubjectNotFoundError(f"Subject {subject} not found")
 
     def load(self) -> Graph:
-        triples = Graph()
+        with self.__lock:
+            triples = Graph()
 
-        for file in os.listdir(os.path.join(self.__store_path, "triples")):
-            try:
-                g = Graph().parse(self.hash_triples_path(file), format="turtle")
-            except Exception as e:
-                from abi import logger
+            for file in os.listdir(os.path.join(self.__store_path, "triples")):
+                try:
+                    g = Graph().parse(self.hash_triples_path(file), format="turtle")
+                except Exception as e:
+                    from abi import logger
 
-                logger.error(
-                    f"Error loading triples from {self.hash_triples_path(file)}: {e}"
-                )
-                raise e
-
-            for prefix, namespace in g.namespaces():
-                triples.bind(prefix, namespace)
-
-            triples += g
-
-        return triples
-
-    def query(self, query: str) -> query.Result:
-        aggregate_graph = self.get()
-
-        return aggregate_graph.query(query)
-
-    def query_view(self, view: str, query: str) -> rdflib.query.Result:
-        if os.path.exists(os.path.join(self.__store_path, "views", view)):
-            aggregate_graph = Graph()
-
-            for file in os.listdir(os.path.join(self.__store_path, "views", view)):
-                g = Graph().parse(
-                    os.path.join(self.__store_path, "views", view, file),
-                    format="turtle",
-                )
+                    logger.error(
+                        f"Error loading triples from {self.hash_triples_path(file)}: {e}"
+                    )
+                    raise e
 
                 for prefix, namespace in g.namespaces():
-                    aggregate_graph.bind(prefix, namespace)
+                    triples.bind(prefix, namespace)
 
-                aggregate_graph += g
+                triples += g
+
+            return triples
+
+    def query(self, query: str) -> query.Result:
+        with self.__lock:
+            aggregate_graph = self.get()
 
             return aggregate_graph.query(query)
-        else:
-            raise Exceptions.ViewNotFoundError(f"View {view} not found")
+
+    def query_view(self, view: str, query: str) -> rdflib.query.Result:
+        with self.__lock:
+            if os.path.exists(os.path.join(self.__store_path, "views", view)):
+                aggregate_graph = Graph()
+
+                for file in os.listdir(os.path.join(self.__store_path, "views", view)):
+                    g = Graph().parse(
+                        os.path.join(self.__store_path, "views", view, file),
+                        format="turtle",
+                    )
+
+                    for prefix, namespace in g.namespaces():
+                        aggregate_graph.bind(prefix, namespace)
+
+                    aggregate_graph += g
+
+                return aggregate_graph.query(query)
+            else:
+                raise Exceptions.ViewNotFoundError(f"View {view} not found")
 
     def handle_view_event(
         self,
