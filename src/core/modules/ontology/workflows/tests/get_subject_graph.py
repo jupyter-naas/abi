5abac2a4449eaba848a77827205a1aef46aa13da
FlorentLvr <florent@naas.ai>
2025-06-06 12:43:15 +0200

    refactor: workflows

--- src/core/modules/ontology/workflows/tests/get_subject_graph.py
+++
@@ -1,9 +0,0 @@
-from abi.utils.SPARQL import get_subject_graph
-
-def test_get_subject_graph(uri: str, depth: int = 1):
-    graph = get_subject_graph(uri, depth=depth)
-    return graph
-
-if __name__ == "__main__":
-    graph = test_get_subject_graph("http://ontology.naas.ai/abi/4d4e6bc4-b396-4d26-b42b-3d257cde1738", depth=2)
-    print(graph.serialize(format="turtle"))
---
+++ src/core/modules/ontology/workflows/tests/test_GetSubjectGraphWorkflow.py
@@ -0,0 +1,23 @@
+from src.core.modules.ontology.workflows.GetSubjectGraphWorkflow import (
+    GetSubjectGraphWorkflow,
+    GetSubjectGraphWorkflowConfiguration,
+    GetSubjectGraphWorkflowParameters
+)
+from src import services
+
+# Configurations
+get_subject_graph_workflow_configuration = GetSubjectGraphWorkflowConfiguration()
+
+# Workflow
+get_subject_graph_workflow = GetSubjectGraphWorkflow(
+    configuration=get_subject_graph_workflow_configuration
+)
+
+# Test
+result = get_subject_graph_workflow.get_subject_graph(
+    GetSubjectGraphWorkflowParameters(
+        uri="http://ontology.naas.ai/abi/4d4e6bc4-b396-4d26-b42b-3d257cde1738",
+        depth=2
+    )
+)
+print(result)
