ba943cb5c0d4fbd70416604b40534701060589b4
FlorentLvr <florent@naas.ai>
2025-06-09 11:29:14 +0200

    refactor: cleaning marketplace

--- marketplace/modules/__archive__/models/naas/Claude_35_SonnetAIModel.py
+++
@@ -1,7 +0,0 @@
-ID = "d2fd72cf-d417-45e0-8388-c38dfb6113a2"
-NAME = "anthropic.claude-3-5-sonnet-20240620-v1:0"
-DESCRIPTION = "200k context window, best for coding and complex task"
-IMAGE = "https://naasai-public.s3.eu-west-3.amazonaws.com/logos/anthropic_100x100.jpg"
-MODEL_TYPE = "chat"
-CONTEXT_WINDOW = 200000
-OWNER = "anthropic"
--- marketplace/modules/__archive__/models/naas/Mistral_7b_InstructAIModel.py
+++
@@ -1,7 +0,0 @@
-ID = "4132275e-2377-4d22-bd0e-db6cda2d3104"
-NAME = "mistral.mistral-7b-instruct-v0:2"
-DESCRIPTION = "A 7B transformer model, fast-deployed and easily customisable. Small, yet very powerful for a variety of use cases."
-IMAGE = "https://naasai-public.s3.eu-west-3.amazonaws.com/logos/mistralai_100x100.jpg"
-MODEL_TYPE = "chat"
-CONTEXT_WINDOW = 32000
-OWNER = "mistral"
