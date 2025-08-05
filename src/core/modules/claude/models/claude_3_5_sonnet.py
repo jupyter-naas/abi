197362a4670f0f370b277f034676b0834192ce77
FlorentLvr <florent@naas.ai>
2025-08-05 16:23:24 +0200

    refactor: claude  models, agent + add Agent_test.py

--- src/core/modules/claude/models/claude_3_5_sonnet.py
+++ src/core/modules/claude/models/claude_3_5_sonnet.py
@@ -1,7 +1,8 @@
 from lib.abi.models.Model import ChatModel
 from langchain_anthropic import ChatAnthropic
 from src import secret
-import os
+from pydantic import SecretStr
+from abi import logger
 
 ID = "claude-3-5-sonnet-20241022"
 NAME = "claude-3-5-sonnet"
@@ -9,23 +10,28 @@ DESCRIPTION = "Anthropic's most intelligent model, with best-in-class performanc
 IMAGE = "https://assets.anthropic.com/m/0edc05fa8e30f2f9/original/Anthropic_Glyph_Black.svg"
 CONTEXT_WINDOW = 200000
 OWNER = "anthropic"
+TEMPERATURE = 0
+MAX_TOKENS = 4096
+MAX_RETRIES = 2
 
-if "ANTHROPIC_API_KEY" not in os.environ:
-    os.environ["ANTHROPIC_API_KEY"] = secret.get("ANTHROPIC_API_KEY", "")
-
-model = ChatModel(
-    model_id=ID,
-    name=NAME,
-    description=DESCRIPTION,
-    image=IMAGE,
-    owner=OWNER,
-    model=ChatAnthropic(
-        model_name=ID,
-        temperature=0,
-        max_tokens_to_sample=4096,
-        timeout=None,
-        max_retries=2,
-        stop=None,
-    ),
-    context_window=CONTEXT_WINDOW,
-) 
+anthropic_api_key = secret.get("ANTHROPIC_API_KEY")
+if anthropic_api_key:
+    model = ChatModel(
+        model_id=ID,
+        name=NAME,
+        description=DESCRIPTION,
+        image=IMAGE,
+        owner=OWNER,
+        model=ChatAnthropic(
+            model_name=ID,
+            temperature=TEMPERATURE,
+            max_tokens_to_sample=MAX_TOKENS,
+            timeout=None,
+            max_retries=MAX_RETRIES,
+            stop=None,
+            api_key=SecretStr(anthropic_api_key),
+        ),
+        context_window=CONTEXT_WINDOW,
+    ) 
+else:
+    logger.error("Anthropic API key not found")
