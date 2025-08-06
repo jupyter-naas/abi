f1eae54de894c80457b8810abd1f9d75812bb41a
FlorentLvr <florent@naas.ai>
2025-08-06 11:34:37 +0200

    feat: push templates module v0 from docs

---
+++ src/core/modules/__templates__/integrations/TemplateIntegration_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.core.modules.__templates__.integrations.TemplateIntegration import (
+    YourIntegration, 
+    YourIntegrationConfiguration,
+)
+
+@pytest.fixture
+def integration() -> YourIntegration:
+    return YourIntegration(YourIntegrationConfiguration(
+        attribute_1="value1",
+        attribute_2=42
+    ))
+
+def test_integration_name(integration):
+    result = integration.example_method("value1")
+
+    assert result is not None, result
+    assert "value1" in result, result
