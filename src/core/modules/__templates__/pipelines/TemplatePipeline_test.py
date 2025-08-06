65edb197c0a8119f58c45351a248d4f8cdc9ff51
FlorentLvr <florent@naas.ai>
2025-08-06 11:43:38 +0200

    feat: improve test and templates + fix error ruff

--- src/core/modules/__templates__/pipelines/TemplatePipeline_test.py
+++ src/core/modules/__templates__/pipelines/TemplatePipeline_test.py
@@ -11,20 +11,23 @@ def pipeline() -> YourPipeline:
     from src.core.modules.__templates__.integrations.TemplateIntegration import (
         YourIntegrationConfiguration
     )
+    from src import services
 
-    integration_configuration = YourIntegrationConfiguration(
-        attribute_1="value1",
-        attribute_2=42
-    )
+    integration_configuration = YourIntegrationConfiguration()
 
     pipeline_configuration = YourPipelineConfiguration(
         integration_configuration=integration_configuration,
+        triple_store=services.triple_store,
     )
 
     return YourPipeline(pipeline_configuration)
 
 def test_pipeline_name(pipeline: YourPipeline):
-    result = pipeline.run(YourPipelineParameters(parameter_1="value1", parameter_2=42))
+    graph = pipeline.run(YourPipelineParameters(parameter_1="value1", parameter_2=42))
 
-    assert result is not None, result
-    assert "value1" in result, result
+    assert graph is not None, graph
+
+    for s, p, o in graph:
+        assert s is not None, s
+        assert p is not None, p
+        assert o is not None, o
