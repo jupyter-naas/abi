04c1e35723c76c8ed2caee38e77382e089aad99a
Maxime Jublou <jubloum@gmail.com>
2025-09-04 16:12:50 +0200

    fix: Create storage dir if none found

--- lib/abi/services/cache/CacheFactory.py
+++ lib/abi/services/cache/CacheFactory.py
@@ -1,7 +1,7 @@
 from abi.services.cache.CacheService import CacheService
 from abi.services.cache.adapters.secondary.CacheFSAdapter import CacheFSAdapter
 
-from abi.utils.Storage import find_storage_folder
+from abi.utils.Storage import find_storage_folder, NoStorageFolderFound
 import os
 
 class CacheFactory:
@@ -9,4 +9,10 @@ class CacheFactory:
     def CacheFS_find_storage(subpath: str = "cache", needle: str = "storage") -> CacheService:
         if not subpath.startswith("cache"):
             subpath = os.path.join("cache", subpath)
-        return CacheService(CacheFSAdapter(os.path.join(find_storage_folder(os.getcwd(), needle), subpath)))
+        
+        try:
+            return CacheService(CacheFSAdapter(os.path.join(find_storage_folder(os.getcwd(), needle), subpath)))
+        except NoStorageFolderFound as _:
+            # Create a "storage" folder for the cache
+            os.makedirs(os.path.join(os.getcwd(), "storage"), exist_ok=True)
+            return CacheService(CacheFSAdapter(os.path.join(find_storage_folder(os.getcwd(), needle), subpath)))
