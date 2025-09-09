e3297009d9480dafed3eb7f21bb32f2057541254
FlorentLvr <florent@naas.ai>
2025-09-09 16:44:41 +0200

    feat: add module to integrate exchangeratesapi

---
+++ src/marketplace/modules/applications/exchangeratesapi/integrations/ExchangeratesapiIntegration_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.marketplace.modules.applications.exchangeratesapi.integrations.ExchangeratesapiIntegration import (
+    ExchangeratesapiIntegration,
+    ExchangeratesapiIntegrationConfiguration,
+)
+from src import secret
+
+
+@pytest.fixture
+def integration() -> ExchangeratesapiIntegration:
+    configuration = ExchangeratesapiIntegrationConfiguration(api_key=secret.get("EXCHANGERATESAPI_API_KEY"))
+    return ExchangeratesapiIntegration(configuration)
+
+def test_list_symbols(integration: ExchangeratesapiIntegration):
+    symbols = integration.list_symbols()
+
+    assert symbols is not None, symbols
+    assert symbols.get("success") is True, symbols
+    assert len(symbols.get("symbols")) > 0, symbols
+
+def test_get_exchange_rates(integration: ExchangeratesapiIntegration):
+    rates = integration.get_exchange_rates(date="2024-12-31", base="EUR", symbols=[])
+
+    assert rates is not None, rates
+    assert rates.get("success") is True, rates
+    assert rates.get("base") == "EUR", rates
+    assert rates.get("date") == "2024-12-31", rates
+    assert len(rates.get("rates")) > 0, rates
