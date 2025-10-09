680fcd46290a3d75275eaa9f5c8ec12bc00f8be8
FlorentLvr <florent@naas.ai>
2025-10-09 15:29:49 +0200

    fix: secrets not needed

--- src/marketplace/applications/linkedin_sales_navigator/__init__.py
+++ src/marketplace/applications/linkedin_sales_navigator/__init__.py
@@ -1,10 +1,2 @@
-from src import secret
-
 def requirements():
-    li_at = secret.get('li_at')
-    JSESSIONID = secret.get('JSESSIONID')
-    li_a = secret.get('li_a')
-    linkedin_profile_url = secret.get('LINKEDIN_PROFILE_URL')
-    if li_at is not None and JSESSIONID is not None and li_a is not None and linkedin_profile_url is not None:
-        return True
     return False
