1448470ebb8e07dfbede9e7e7eda742a5a1b173f
FlorentLvr <florent@naas.ai>
2025-11-03 19:05:08 +0100

    feat: add class OpenRouter

---
+++ lib/abi/services/agent/OpenRouter.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI  # using the OpenAI LLM class as wrapper
+from pydantic import SecretStr
+
+class ChatOpenRouter(ChatOpenAI):
+    def __init__(self, model_name: str, **kwargs):
+        api_key = SecretStr(os.environ["OPENROUTER_API_KEY"])
+        print(api_key)
+        super().__init__(
+            model=model_name,
+            api_key=api_key,
+            temperature=0,
+            base_url="https://openrouter.ai/api/v1",
+            **kwargs,
+        )
