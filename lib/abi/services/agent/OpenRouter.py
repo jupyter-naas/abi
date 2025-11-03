131d02ca8e5d52010f664ebcae746af0150832e0
FlorentLvr <florent@naas.ai>
2025-11-03 19:07:49 +0100

    refactor: rm print

--- lib/abi/services/agent/OpenRouter.py
+++ lib/abi/services/agent/OpenRouter.py
@@ -8,11 +8,9 @@ from pydantic import SecretStr
 class ChatOpenRouter(ChatOpenAI):
     def __init__(self, model_name: str, **kwargs):
         api_key = SecretStr(os.environ["OPENROUTER_API_KEY"])
-        print(api_key)
         super().__init__(
             model=model_name,
             api_key=api_key,
-            temperature=0,
             base_url="https://openrouter.ai/api/v1",
             **kwargs,
         )
