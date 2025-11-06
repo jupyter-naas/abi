4be029b39bd6870f9a0b810931bf32f2e6ab182f
FlorentLvr <florent@naas.ai>
2025-11-06 13:05:50 +0100

    fix: error load default models

--- src/core/__templates__/models/module_default.py
+++ src/core/__templates__/models/module_default.py
@@ -1,14 +1,14 @@
 from src import secret
 from lib.abi.models.Model import ChatModel
-from src.core.abi.models.airgap_qwen import model as airgap_model
-from src.core.chatgpt.models.gpt_4_1_mini import model as cloud_model
 
 model: ChatModel
+airgap_model: ChatModel
+cloud_model: ChatModel
 ai_mode = secret.get("AI_MODE")
-airgap_model: ChatModel = airgap_model
-cloud_model: ChatModel = cloud_model
 
 if ai_mode == "airgap":
+    from src.core.abi.models.airgap_qwen import model as airgap_model
     model = airgap_model
 else:
+    from src.core.chatgpt.models.gpt_4_1_mini import model as cloud_model
     model = cloud_model
