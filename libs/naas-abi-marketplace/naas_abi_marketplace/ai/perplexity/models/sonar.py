8161093976c9de698b559d2714bd50144a756354
FlorentLvr <florent@naas.ai>
2025-12-16 10:23:04 +0100

    refactor: ai/perplexity module

--- libs/naas-abi-marketplace/naas_abi_marketplace/ai/perplexity/models/sonar.py
+++ libs/naas-abi-marketplace/naas_abi_marketplace/ai/perplexity/models/sonar.py
@@ -1,14 +1,12 @@
-from langchain_perplexity import ChatPerplexity
-from naas_abi import secret
+from langchain_perplexity import ChatPerplexity  # type: ignore
+from naas_abi_marketplace.ai.perplexity import ABIModule
 from naas_abi_core.models.Model import ChatModel
 from pydantic import SecretStr
 
 MODEL_ID = "sonar"
 PROVIDER = "perplexity"
 
-api_key = secret.get("PERPLEXITY_API_KEY")
-if secret.get("OPENROUTER_API_KEY"):
-    api_key = secret.get("OPENROUTER_API_KEY")
+api_key = ABIModule.get_instance().configuration.perplexity_api_key or ""
 
 model: ChatModel = ChatModel(
     model_id=MODEL_ID,
