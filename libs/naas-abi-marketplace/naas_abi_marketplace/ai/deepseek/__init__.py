c32d56405e8da57958926abe9860369cea948913
FlorentLvr <florent@naas.ai>
2025-12-30 18:55:31 +0100

    refactor: init file ai modules local

--- libs/naas-abi-marketplace/naas_abi_marketplace/ai/deepseek/__init__.py
+++ libs/naas-abi-marketplace/naas_abi_marketplace/ai/deepseek/__init__.py
@@ -1,2 +1,26 @@
-def requirements():
-    return True
+from naas_abi_core.module.Module import (
+    BaseModule,
+    ModuleConfiguration,
+    ModuleDependencies,
+)
+from naas_abi_core.services.object_storage.ObjectStorageService import (
+    ObjectStorageService,
+)
+
+
+class ABIModule(BaseModule):
+    dependencies: ModuleDependencies = ModuleDependencies(
+        modules=[],
+        services=[ObjectStorageService],
+    )
+
+    class Configuration(ModuleConfiguration):
+        """
+        Configuration example:
+
+        module: naas_abi_marketplace.ai.deepseek
+        enabled: true
+        config:
+            datastore_path: "deepseek"
+        """
+        datastore_path: str = "deepseek"
