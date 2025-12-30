dd436e91056cf54e9107b38e7b47b47b6d7b717a
FlorentLvr <florent@naas.ai>
2025-12-30 18:40:37 +0100

    feat: init google modules

---
+++ libs/naas-abi-marketplace/naas_abi_marketplace/applications/google_calendar/__init__.py
@@ -0,0 +1,27 @@
+from naas_abi_core.module.Module import (
+    BaseModule,
+    ModuleConfiguration,
+    ModuleDependencies,
+)
+from naas_abi_core.services.object_storage.ObjectStorageService import (
+    ObjectStorageService,
+)
+
+
+class ABIModule(BaseModule):
+    dependencies: ModuleDependencies = ModuleDependencies(
+        modules=[],
+        services=[ObjectStorageService],
+    )
+
+    class Configuration(ModuleConfiguration):
+        """
+        Configuration example:
+
+        module: naas_abi_marketplace.applications.google_calendar
+        enabled: true
+        config:
+            datastore_path: "google_calendar"
+        """
+        datastore_path: str = "google_calendar"
+
---
+++ libs/naas-abi-marketplace/naas_abi_marketplace/applications/google_drive/__init__.py
@@ -0,0 +1,27 @@
+from naas_abi_core.module.Module import (
+    BaseModule,
+    ModuleConfiguration,
+    ModuleDependencies,
+)
+from naas_abi_core.services.object_storage.ObjectStorageService import (
+    ObjectStorageService,
+)
+
+
+class ABIModule(BaseModule):
+    dependencies: ModuleDependencies = ModuleDependencies(
+        modules=[],
+        services=[ObjectStorageService],
+    )
+
+    class Configuration(ModuleConfiguration):
+        """
+        Configuration example:
+
+        module: naas_abi_marketplace.applications.google_drive
+        enabled: true
+        config:
+            datastore_path: "google_drive"
+        """
+        datastore_path: str = "google_drive"
+
