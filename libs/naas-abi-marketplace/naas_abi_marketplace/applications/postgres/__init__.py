aa36fcc4c64c84dc27c726fb3769fe7ad171e4d6
FlorentLvr <florent@naas.ai>
2025-12-30 17:45:56 +0100

    refactor: postgres module

--- libs/naas-abi-marketplace/naas_abi_marketplace/applications/postgres/__init__.py
+++ libs/naas-abi-marketplace/naas_abi_marketplace/applications/postgres/__init__.py
@@ -1,19 +1,35 @@
-from naas_abi import secret
+from naas_abi_core.module.Module import (
+    BaseModule,
+    ModuleConfiguration,
+    ModuleDependencies,
+)
+from naas_abi_core.services.object_storage.ObjectStorageService import (
+    ObjectStorageService,
+)
 
 
-def requirements():
-    # Postgres Configuration
-    POSTGRES_USER = secret.get("POSTGRES_USER")
-    POSTGRES_PASSWORD = secret.get("POSTGRES_PASSWORD")
-    POSTGRES_DBNAME = secret.get("POSTGRES_DBNAME")
-    POSTGRES_HOST = secret.get("POSTGRES_HOST")
-    POSTGRES_PORT = secret.get("POSTGRES_PORT")
-    if (
-        POSTGRES_USER
-        and POSTGRES_PASSWORD
-        and POSTGRES_DBNAME
-        and POSTGRES_HOST
-        and POSTGRES_PORT
-    ):
-        return True
-    return False
+class ABIModule(BaseModule):
+    dependencies: ModuleDependencies = ModuleDependencies(
+        modules=[],
+        services=[ObjectStorageService],
+    )
+
+    class Configuration(ModuleConfiguration):
+        """
+        Configuration example:
+
+        module: naas_abi_marketplace.applications.postgres
+        enabled: true
+        config:
+            postgres_user: "{{ secret.POSTGRES_USER }}"
+            postgres_password: "{{ secret.POSTGRES_PASSWORD }}"
+            postgres_dbname: "{{ secret.POSTGRES_DBNAME }}"
+            postgres_host: "{{ secret.POSTGRES_HOST }}"
+            postgres_port: "{{ secret.POSTGRES_PORT }}"
+        """
+        postgres_user: str
+        postgres_password: str
+        postgres_dbname: str
+        postgres_host: str
+        postgres_port: str
+        datastore_path: str = "postgres"
