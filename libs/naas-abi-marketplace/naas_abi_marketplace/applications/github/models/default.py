a97ebd19724754d0d023b3e77fa0c41877b65194
FlorentLvr <florent@naas.ai>
2026-01-02 15:58:10 +0100

    fix: github default model

--- libs/naas-abi-marketplace/naas_abi_marketplace/applications/github/models/default.py
+++ libs/naas-abi-marketplace/naas_abi_marketplace/applications/github/models/default.py
@@ -5,15 +5,16 @@ from naas_abi_marketplace.applications.github import ABIModule
 
 
 def get_model() -> ChatModel:
+    from naas_abi_marketplace.ai.chatgpt import ABIModule as ChatGPTABIModule
+
     ai_mode: Literal["cloud", "local", "airgap"] = (
         ABIModule.get_instance().configuration.global_config.ai_mode
     )
     if (
         ai_mode == "airgap"
-        or not ABIModule.get_instance().configuration.openai_api_key
-        and not ABIModule.get_instance().configuration.openrouter_api_key
+        or not ChatGPTABIModule.get_instance().configuration.openai_api_key
     ):
-        from naas_abi_marketplace.ai.qwen.models.qwen3_8b import model as airgap_model
+        from naas_abi.models.airgap_qwen import model as airgap_model
 
         return airgap_model
     else:
