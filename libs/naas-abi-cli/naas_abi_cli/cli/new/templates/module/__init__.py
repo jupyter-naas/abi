6753990aae06a44f164ef06e84db06d8acfd51f9
Maxime Jublou <jubloum@gmail.com>
2026-01-25 22:36:23 +0100

    feat: Add more documentation on the generate code

--- libs/naas-abi-cli/naas_abi_cli/cli/new/templates/module/__init__.py
+++ libs/naas-abi-cli/naas_abi_cli/cli/new/templates/module/__init__.py
@@ -27,5 +27,15 @@ class ABIModule(BaseModule):
         pass
         # example: str
 
+    # on_initialized is called by the engine after all modules and services have been fully loaded.
+    # At this point, you can safely access other modules and services through the engine's interfaces.
+    # Override this method to implement any post-initialization logic your module requires.
     def on_initialized(self):
         super().on_initialized()
+
+    # The on_load method is invoked during initial module loading by the engine.
+    # At this point, avoid accessing services or other modules, as they have not been loaded yet.
+    # Place any logic here that must occur right as the module is loaded, before initialization.
+    # You can see it as the constructor of the module.
+    def on_load(self):
+        super().on_load()
