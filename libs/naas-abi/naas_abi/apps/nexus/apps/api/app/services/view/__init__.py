2cc04f9ccd6d1a50dc722b78847a59fed187871d
FlorentRvl <florent@naas.ai>
2026-04-28 12:03:39 +0200

    refactor: view endpoint to hexa

---
+++ libs/naas-abi/naas_abi/apps/nexus/apps/api/app/services/view/__init__.py
@@ -0,0 +1,11 @@
+from naas_abi.apps.nexus.apps.api.app.services.view.service import (
+    ViewNotFoundError,
+    ViewService,
+    ViewServiceUnavailableError,
+)
+
+__all__ = [
+    "ViewNotFoundError",
+    "ViewService",
+    "ViewServiceUnavailableError",
+]
