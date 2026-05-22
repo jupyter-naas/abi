03443ea8b4e08cb4fa56c19ddb5bbc7d53eed268
FlorentRvl <florent@naas.ai>
2026-05-22 13:29:47 +0200

    refactor: update nexus ontologies

---
+++ libs/naas-abi/naas_abi/ontologies/classes/ontology_naas_ai/abi/DocumentContentEntity.py
@@ -0,0 +1,11 @@
+from naas_abi.ontologies.modules.ABIOntology import (
+    DocumentContentEntity as _DocumentContentEntity,
+)
+
+
+class DocumentContentEntity(_DocumentContentEntity):
+    """Action class for DocumentContentEntity"""
+
+    def actions(self):
+        """Action method - implement your logic here"""
+        pass
