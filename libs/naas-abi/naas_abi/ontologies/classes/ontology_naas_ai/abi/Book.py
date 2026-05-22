f0267345ae6b0520c1e5604d0a9a225a64402201
FlorentRvl <florent@naas.ai>
2026-05-22 13:29:47 +0200

    refactor: update nexus ontologies

---
+++ libs/naas-abi/naas_abi/ontologies/classes/ontology_naas_ai/abi/Book.py
@@ -0,0 +1,11 @@
+from naas_abi.ontologies.modules.ABIOntology import (
+    Book as _Book,
+)
+
+
+class Book(_Book):
+    """Action class for Book"""
+
+    def actions(self):
+        """Action method - implement your logic here"""
+        pass
---
+++ libs/naas-abi/naas_abi/ontologies/classes/ontology_naas_ai/abi/Report.py
@@ -0,0 +1,11 @@
+from naas_abi.ontologies.modules.ABIOntology import (
+    Report as _Report,
+)
+
+
+class Report(_Report):
+    """Action class for Report"""
+
+    def actions(self):
+        """Action method - implement your logic here"""
+        pass
